514b9ffa6267993e0bc35c48a3a9024dd616233e
NULLCT <cockatiel.u10@gmail.com>
2022-02-20 16:09:45 +0900

    add source

---
+++ stldumper.hpp
@@ -0,0 +1,124 @@
+//       _   _     _                                   //
+//   ___| |_| | __| |_   _ _ __ ___  _ __   ___ _ __   //
+//  / __| __| |/ _` | | | | '_ ` _ \| '_ \ / _ \ '__|  //
+//  \__ \ |_| | (_| | |_| | | | | | | |_) |  __/ |     //
+//  |___/\__|_|\__,_|\__,_|_| |_| |_| .__/ \___|_|     //
+//                                  |_|                //
+
+#include <ostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <tuple>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+template <class T>
+ostream &operator<<(ostream &_ostr, const vector<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const deque<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const list<T> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v);
+template <class... Ts>
+ostream &operator<<(ostream &_ostr, const tuple<Ts...> &t);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const map<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_multimap<T, Y> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const set<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const multiset<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v);
+
+template <class T>
+void orange(ostream &_ostr, const T &_v) {
+  if (_v.size() == 0)
+    return;
+  _ostr << *_v.begin();
+  for (auto itr = next(_v.begin()); itr != _v.end(); itr++)
+    _ostr << " " << *itr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const vector<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const deque<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const list<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v) {
+  _ostr << _v.first << " " << _v.second;
+  return _ostr;
+}
+template <class... Ts>
+ostream &operator<<(ostream &_ostr, const tuple<Ts...> &_v) {
+  bool first = true;
+  apply([&_ostr, &first](auto &&... args) {
+    auto print = [&](auto &&val) {
+      if (!first)
+        _ostr << " ";
+      (_ostr << val);
+      first = false;
+    };
+    (print(args), ...);
+  },
+        _v);
+  return _ostr;
+}
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const map<T, Y> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const set<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const multiset<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v) {
+  orange(_ostr, _v);
+  return _ostr;
+}
